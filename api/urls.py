b4e39306e222fcbe4b5807fa3f73e47cc01cfda5
Shin-002 <anko13ko@yahoo.co.jp>
2020-05-25 05:59:45 +0900

    CreateView pathとviewの紐付

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from django.conf.urls import include
 from rest_framework import routers
+from .views import TaskViewSet, UserViewSet, SprintViewSet, TagViewSet
 
 router = routers.DefaultRouter()
+router.register('users', UserViewSet)
+router.register('tasks', TaskViewSet)
+router.register('sprints', SprintViewSet)
+router.register('tags', TagViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from .models import Task, Sprint, Tag
+from .serializers import TaskSerializer, SprintSerializer, TagSerializer, UserSerializers
+from rest_framework import viewsets
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    class SprintViewSet(viewsets.ModelViewSet):
+    queryset = Sprint.objects.all()
+    serializer_class = SprintSerializer
+
+    class TagViewSet(viewsets.ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+    class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
